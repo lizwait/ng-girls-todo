9a60a63f34aa855d7e3521c6af0f77915f584cbc
Elizabeth Wait <Elizabeth.Wait@vmlyr.com>
2020-07-25 13:55:42 -0500

    Adding inputElementRef variable

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,14 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   template: `
-  <p>
-    input-button-unit works!
-    The title is: {{ title }}
-  </p>
-
-  <input [value]="title"
+  <input #inputElementRef
+         [value]="title"
          (keyup.enter)="changeTitle($event.target.value)">
-  <button (click)="changeTitle('Button Clicked!')">Save</button>
+  <button (click)="changeTitle(inputElementRef.value)">Save</button>
 `,
   styleUrls: ['./input-button-unit.component.css']
 })
