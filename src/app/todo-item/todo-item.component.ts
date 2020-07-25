7cfa7d13e3a93fcfb488bb22f72f75c9767233ad
Elizabeth Wait <Elizabeth.Wait@vmlyr.com>
2020-07-25 15:05:22 -0500

    Adding CSS

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -3,7 +3,9 @@ import { TodoItem } from '../interfaces/todo-item';
 @Component({
   selector: 'app-todo-item',
   template: `
-    {{ item.title }}
+   <div class="todo-item">
+     {{ item.title }}
+   </div>
   `,
   styleUrls: ['./todo-item.component.css']
 })
